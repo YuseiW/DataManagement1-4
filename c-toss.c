5125f5ec3c90d91e35da768bfc83c54633ce967a
YuseiW <s1310154@u-aizu.ac.jp>
2024-07-04 17:22:08 +0900

    HoT: initial revision

---
+++ c-toss.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    // Seed the random number generator
+    srand(time(NULL));
+
+    int headsCount = 0;
+    int tailsCount = 0;
+
+    printf("Tossing a coin...\n");
+
+    for (int i = 1; i <= 3; i++) {
+        int coin = rand() % 2; // Generates 0 or 1
+        if (coin == 0) {
+            printf("Round %d: Heads\n", i);
+            headsCount++;
+        } else {
+            printf("Round %d: Tails\n", i);
+            tailsCount++;
+        }
+    }
+
+    printf("Heads: %d, Tails: %d\n", headsCount, tailsCount);
+
+    return 0;
+}
